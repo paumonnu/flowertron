0e6f854d24a60105507596f5a95ff9fb1e2c11fa
Pau Monserrat Nunez <pau.monserrat.nu@gmail.com>
2025-06-17 16:53:01 +0200

    Fix

--- arduino/flowertron/flowertron.ino
+++ arduino/flowertron/flowertron.ino
@@ -15,10 +15,10 @@ unsigned long interval = 20;
 // int prevPin3 = LOW;
 int prevEncoder = 0;
 int prevPin4 = LOW;
-int prevPin5 = LOW;
 int prevPin6 = LOW;
-int prevPin7 = LOW;
 int prevPin9 = LOW;
+int prevPin10 = LOW;
+int prevPin11 = LOW;
 bool zoomed = false;
 
 BasicEncoder encoder(2, 3);
@@ -36,10 +36,10 @@ void setup() {
   // pinMode(2, INPUT);
   //pinMode(3, INPUT);
   pinMode(4, INPUT);
-  pinMode(5, INPUT);
   pinMode(6, INPUT);
-  pinMode(7, INPUT);
   pinMode(9, INPUT);
+  pinMode(10, INPUT);
+  pinMode(11, INPUT);
 
   timer = millis();
 }
@@ -48,10 +48,10 @@ void loop() {
   if (millis() - timer >= interval) {
     timer = millis();
     int pin4 = digitalRead(4);
-    int pin5 = digitalRead(5);
     int pin6 = digitalRead(6);
-    int pin7 = digitalRead(7);
     int pin9 = digitalRead(9);
+    int pin10 = digitalRead(10);
+    int pin11 = digitalRead(11);
 
     int encoderChange = encoder.get_change();
     if (encoderChange) {
@@ -70,22 +70,23 @@ void loop() {
       Serial.println("P");
     }
 
-    if (pin5 != prevPin5 && pin5 == HIGH) {
-      Serial.println("Z"); 
-    }
-
-    if (pin7 != prevPin7 && pin7 == HIGH) {
-      Serial.println("O");
-    }
-
     if (pin9 != prevPin9) {
       Serial.println("I");
     }
+    
+    if (pin10 != prevPin10 && pin10 == HIGH) {
+      Serial.println("O");
+    }
+    
+    if (pin11 != prevPin11 && pin11 == HIGH) {
+      Serial.println("Z"); 
+    }
+    
 
     prevPin4 = pin4;
-    prevPin5 = pin5;
+    prevPin11 = pin11;
     prevPin6 = pin6;
-    prevPin7 = pin7;
+    prevPin10 = pin10;
     prevPin9 = pin9;
   }
 }
